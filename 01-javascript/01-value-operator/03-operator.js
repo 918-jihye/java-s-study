4906fc485a1e60577dfeea6dd06f8bfb2ae7a97a
Wiz <sjkimwiz@gmail.com>
2025-08-22 15:37:23 +0900

    fix

--- 01-javascript/01-value-operator/03-operator.js
+++ 01-javascript/01-value-operator/03-operator.js
@@ -1,63 +1,70 @@
-// 1. 기본 연산자
-// =
-// +, -, *, /, %
-// +=, -=, *=, /=, %=
-// ++, --
+// 연산자(Operator)
 
-// 2. 비교 연산자
-// ===, !==
-// <, <=, >, >=
+/*
+기본 연산자
+=
++, -, *, /, %
++=, -=, *=, /=, %=
+++, --
 
-// 3. 논리 연산자
-// && (AND, 두 조건이 모두 true일 때만 true)
-// || (OR, 두 조건 중 하나라도 true이면 true)
-// ! (NOT, 논리 값을 반대로 변경)(0, "", null, undefined, NaN는 false로 취급)
+비교 연산자
+===, !==
+<, <=, >, >=
 
-// 4. 타입 체크 연산자: typeof
-// null: object로 반환됩니다. null인지 확인하려면 myNull === null처럼 엄격한 비교를 사용하는 것이 좋습니다.
-// array: object로 반환됩니다. 배열인지 확인하려면 Array.isArray() 메서드를 사용하는 것이 더 정확합니다.
-const myString = "hello";
-console.log(typeof myString); // "string"
+논리 연산자
+&& (AND, 두 조건이 모두 true일 때 true)
+|| (OR, 두 조건 중 하나라도 true이면 true)
+! (NOT, 논리 값을 반대로 변환)
+
+삼항 연산자(조건이 참이면 a, 거짓이면 b를 반환)
+조건 ? a : b
+
+옵셔널 체이닝(객체의 속성에 접근할 때 속성이 null 또는 undefined이면 에러를 내지 않고 undefined를 반환)
+.?
+
+널 병합 연산자(왼쪽 값이 null 또는 undefined인 경우 오른쪽 값을 반환)
+??
+
+타입 체크 연산자
+typeof
+  null: object로 확인됩니다. 값이 null인지 확인하려면 if (x === null)를 사용하는 것이 좋습니다.
+  array: object로 확인됩니다. 값이 배열인지 확인하려면 Array.isArray(x)를 사용하는 것이 좋습니다.
+
+상속 체크 연산자
+instanceof
+*/
+const user = { name: "김철수", age: 20 };
+console.log(user.age >= 20 ? "성인" : "청소년");
+console.log(user.phone?.number);
+console.log(user.phone?.number ?? "010-XXX-XXXX");
+
+const myString = "123";
+console.log(typeof myString);
 
 const myNumber = 123;
-console.log(typeof myNumber); // "number"
+console.log(typeof myNumber);
 
 const myBoolean = true;
-console.log(typeof myBoolean); // "boolean"
+console.log(typeof myBoolean);
 
 let myUndefined;
-console.log(typeof myUndefined); // "undefined"
+console.log(typeof myUndefined);
 
 const myNull = null;
-console.log(typeof myNull); // "object" > 자바스크립트 개발자 초기 실수
+console.log(typeof myNull);
 
-const myObject = { name: "John" }; // new Object와 같다.
-console.log(typeof myObject); // "object"
+const myObject = { email: "asd@gmail.com" };
+console.log(typeof myObject);
 
-const myArray = []; // new Array()와 같다. new Object의 자식이다.
-console.log(typeof myArray); // "object"
+const myArray = [];
+console.log(typeof myArray);
 
 const myFunction = function () {};
-console.log(typeof myFunction); // "function", new Object의 자식이다.
+console.log(typeof myFunction);
 
-// 5-1. 특수 연산자: 삼항 연산자
-const user = { name: "김철수", age: 20 };
-
-console.log(user.age >= 20 ? "성인" : "청소년");
-
-// 5-2. 특수 연산자: 옵셔널 체이닝
-// : 속성에 접근할 때, 속성이 null 또는 undefined이면 에러를 내지 않고 undefined를 반환합니다.
-console.log(user.phone?.number); // undefined
-
-// 5-3. 특수 연산자: 널 병합 연산자
-// : 왼쪽 값이 null 또는 undefined인 경우 오른쪽 값을 반환합니다.
-console.log(user.phone?.number ?? "010-XXX-XXXX"); // 010-XXX-XXXX
-
-// 6. 상속 관계 체크 연산자: instanceof
 class Animal {}
 class Dog extends Animal {}
-const myDog = new Dog();
-const myAnimal = new Animal();
-console.log(myDog instanceof Dog); // true
-console.log(myDog instanceof Animal); // true
-console.log(myAnimal instanceof Dog); // false
+const dog = new Dog();
+const animal = new Animal();
+console.log(dog instanceof Animal);
+console.log(dog instanceof Dog);
