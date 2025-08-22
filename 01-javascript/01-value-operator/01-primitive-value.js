ff9ac547fd22b3547b9584fd9a1ff3642ca0aa01
Wiz <sjkimwiz@gmail.com>
2025-08-22 15:26:03 +0900

    fix

--- 01-javascript/01-value-operator/01-primitive-value.js
+++ 01-javascript/01-value-operator/01-primitive-value.js
@@ -1,40 +1,63 @@
 // 원시 값(Primitive Value)
 
-// 변수
-// : 값에 이름을 붙인 것
-// : const(값을 변경할 수 없음, undefined로 값을 초기화할 수 없다)
-// : let(값을 변경할 수 있음, undefined로 값을 초기화할 수 있다)
-// : user_name, userName, UserName: 언어마다 선호하는 형식이 다르다.
+/*
+변수: 값에 이름을 붙인 것
 
-// 타입
-// : 문자열(string): "", '', `${변수}`, "\n"
-// :   문자열 값으로 메소드를 사용하면 런타임에 동적으로 String 객체가 생성되고 사용후 제거된다.
-// :   const address = "    대한민국-서울광역시-강남구     ";
-// :   console.log(address.trim().split("-"));
-// :   출력 결과: ["대한민국", "서울광역시", "강남구"];
-// : 숫자(number): 숫자, NaN, Infinity, -Infinity
-// : 참 또는 거짓(boolean): true, false
-// : 값을 정의하지 않음: undefined
-// : 값이 없음: null
-// : 큰 정수(bigint)
-// : symbol
+const nickname = 1;
+const: 값을 변경할 수 없으며 정의할 때 값을 선언해야 함
 
-// 형변환
-// : String(값)
-// : Number(값)(실패하면 NaN), parseInt(값), parseFloat(값)
-// : Boolean(값): 0, "", null, undefined, NaN = falsy 값
-const userName1 = "김민정";
-const userAge1 = 19;
-console.log(`이름: ${userName1}, 나이: ${userAge1}`);
+let x = 1;
+let: 값을 변경할 수 있으며, 정의할 때 값을 선언하지 않아도 됨
 
-const userName2 = "박지연";
-const userAge2 = 20;
-console.log(`이름: ${userName2}, 나이: ${userAge2}`);
+항상 const를 사용해서 변수를 선언하고, 꼭 필요한 경우 let을 사용
 
-console.log(Boolean(0)); // false
-console.log(Boolean("")); // false
-console.log(Boolean(null)); // false
-console.log(Boolean(undefined)); // false
-console.log(Boolean(NaN)); // faalse
+const isStudent = true; >> javascript, c#, java, swift, go
+const is_student = true; >> python, c, c++
+*/
+const nickname1 = "김민정";
+const age1 = 20;
+const isStudent1 = true;
+console.log(`이름: ${nickname1}, 나이: ${age1}`);
 
-const x = "aksdbkashd";
+const nickname2 = "박진웅";
+const age2 = 30;
+const isStudent2 = false;
+console.log(`이름: ${nickname2}, 나이: ${age2}`);
+
+
+/*
+타입: 값의 형식
+
+문자열(string): "", '', `${변수}`
+  문자열 값으로 메소드를 사용하면 런타임에 동적으로 String 객체가 생성되고 사용후 제거된다.
+  const address = "    대한민국-서울광역시-강남구     ";
+  console.log(address.trim().split("-"));
+  출력 결과: ["대한민국", "서울광역시", "강남구"];
+숫자(number): 숫자, NaN, Infinity, -Infinity
+참 또는 거짓(boolean): true, false
+값을 정의하지 않음: undefined
+값이 없음: null
+큰 정수(bigint)
+symbol
+*/
+
+/*
+타입 변환
+
+문자열로 변환
+  String(값)
+숫자로 변환
+  Number(값) >> 숫자로 변환, 실패하면 NaN
+  parseInt(값) >> 정수로 변환, 실패하면 ?
+  parseFloat(값) >> 소수로 변환, 실패하면 ?
+참 또는 거짓으로 변환
+  Boolean(값)
+  변환하면 false가 되는 것: 0, "", null, undefined, NaN
+  변환하면 true가 되는 것: 위의 5가지를 제외한 나머지 전부
+*/
+
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN)); 
--- 01-javascript/01-value-operator/02-reference-value.js
+++ 01-javascript/01-value-operator/02-reference-value.js
@@ -1,45 +1,72 @@
 // 참조 값(Refefence Value)
 
-// Object
-// : 키와 값의 쌍으로 데이터를 관리합니다.
-// : 조회: user.userName, user["userName"]
-// : spread: ...
-// : destructing
+/*
+객체(Object): 복합적인 형식의 데이터를 키와 값 구조로 담을 수 있는 컨테이너
+키를 통한 값 조회: user1.email, user1["email"]
+키 목록 조회: Object.keys(user1)
+값 목록 조회: Object.keys(user1)
+키와 값 목록 조회: Object.entries(user1)
+*/
 let user1 = {
-  userName: "김민정",
-  userAge: 19,
-};
-user1 = {
-  ...user1,
-  nation: "한국",
-};
+  email: "iu@gmail.com",
+  password: "1234",
+  nickname: "아이유",
+  age: 19,
+  isStudent: true,
+}
 let user2 = {
-  userName: "박지연",
-  userAge: 20,
-};
-user2 = {
-  ...user2,
-  nation: "미국",
-};
-console.log(
-  `이름: ${user1.userName}, 나이: ${user1.userAge}, 국적: ${user1.nation}`,
-);
-console.log(
-  `이름: ${user2["userName"]}, 나이: ${user2["userAge"]}, 국적: ${user2.nation}`,
-);
+  email: "mj@gmail.com",
+  password: "1234",
+  nickname: "마이클 잭슨",
+  age: 35,
+  isStudent: false,
+}
+
+console.log(user1);
+console.log(user1.email);
 console.log(Object.keys(user1));
 console.log(Object.values(user1));
 console.log(Object.entries(user1));
 
-// Array
-// : 여러개의 데이터를 관리합니다.
-// : 길이: users.length
-// : 조회: users[인덱스]
-// : 추가: users.push(요소), users.unshift(요소), users.splice(시작 인덱스, 0, 추가할 요소)
-// : 삭제: users.splice(시작 인덱스, 제거할 개수), users.shift();
-// : spread: ...
-// : destructing: const [element1, element2, element3] = [1, 2, 3];
+console.log(`이메일: ${user1.email}, 별명: ${user1.nickname}, 나이: ${user1.age}`);
+console.log(`이메일: ${user2.email}, 별명: ${user2.nickname}, 나이: ${user2.age}`);
+
+/*
+배열(Array): 여러개의 데이터를 담을 수 있는 컨테이너
+요소 개수: users.length
+요소 조회: users[0]
+요소 추가: users.push(user1), users.unshift(user1), users.splice(인덱스, 0, user1)
+요소 삭제: users.pop(), users.shift(), users.splice(인덱스, 제거할 요소의 개수)
+*/
 const users = [user1];
+console.log(users);
 users.push(user2);
-console.log(`이름: ${users[0].userName}, 나이: ${users[0].userAge}`);
-console.log(`이름: ${users[1].userName}, 나이: ${users[1].userAge}`);
+console.log(users);
+console.log(users[0]);
+console.log(users[1]);
+users[0].hobbies = ["독서"];
+users[1].hobbies = ["노래"];
+
+console.log(`이메일: ${users[0].email}, 별명: ${users[0].nickname}, 나이: ${users[0].age}`);
+console.log(`이메일: ${users[1].email}, 별명: ${users[1].nickname}, 나이: ${users[1].age}`);
+
+
+/*
+객체(Object)
+펼치기: const newUsers = [...users]
+구조분해: const [user1, user2, user3] = users;
+
+배열(Array)
+펼치기: const newUsers = [...users]
+구조분해: const [user1, user2, user3] = users;
+*/
+users[0] = {
+  ...users[0],
+  hobbies: [...users[0].hobbies, "노래"],
+};
+users[1] = {
+  ...users[1],
+  hobbies: [...users[1].hobbies, "춤"],
+};
+console.log(users[0]);
+console.log(users[1]);
